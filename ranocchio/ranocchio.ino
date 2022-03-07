6b6853c9f926f2aba10ac682629bb241f2e1b6dd
picosapiens <99155710+picosapiens@users.noreply.github.com>
2022-03-07 17:17:29 -0500

    Software spi for Mega
    Uno and Mega use different pins. To read SD card without rewiring, have to use Software SPI. This requires modifying Sd2Card.h and .cpp in the Adafruit SD card library from https://github.com/adafruit/SD and define MEGA_SOFT_SPI 1.

--- ranocchio/ranocchio.ino
+++ ranocchio/ranocchio.ino
@@ -69,7 +69,7 @@ uint8_t leftfunc;
 
 File root;
 
-/*
+
 void printDirectory(File dir, int numTabs) {
   while (true) {
 
@@ -92,7 +92,7 @@ void printDirectory(File dir, int numTabs) {
     }
     entry.close();
   }
-}*/
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -144,19 +144,27 @@ void setup() {
   leftfunc = COARSEADJUST;
   triggertype = NOTRIGGER;
 
-  // Mega uses different pins that will need to be sorted out
-  /*
-  //pinMode(10,OUTPUT);
-  //digitalWrite(10, LOW); // Low enable
-  pinMode(A3,OUTPUT);
-  digitalWrite(A3,HIGH); // LCD Disable
+  // Mega uses different pins for hardware SPI. In theory it should be possible to work if you change the pin connections
+  // as shown below. Instead, though, the approach is taken to use software SPI. This requires modifying Sd2Card.h and .cpp
+  // in the Adafruit SD card library from https://github.com/adafruit/SD, and define MEGA_SOFT_SPI 1.
+  // 
+  // SPI  Uno  Mega
+  //  SS  10   53
+  //MOSI  11   51
+  //MISO  12   50
+  // SCK  13   52
+  #warning this will only compile properly if USE_SPI_LIB has been commented out in Sd2Card.h and .cpp, and define MEGA_SOFT_SPI 1
+  //pinMode(53,OUTPUT);
+  //digitalWrite(53, LOW); // Low enable
+  //pinMode(A3,OUTPUT);
+  //digitalWrite(A3,HIGH); // LCD Disable
   //SoftwareSerial ss(8,9)/ % Rx, Tx
-  SDready = SD.begin(10); // Mega uses pins 50-53, so this fails
+  SDready = SD.begin();
   if(!SDready)
     Serial.println("SD card failed to begin");
   root = SD.open("/");
   printDirectory(root, 0);
-  */
+  delay(3000);
 }
 
 void loop() {
